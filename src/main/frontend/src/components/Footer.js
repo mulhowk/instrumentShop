5fc2c0c9b23e439964db9979dad4807f2aadb59f
rkdxogur97 <rkdxogur97@gmail.com>
2023-10-31 16:56:06 +0900

    feat(Footer.js) : add component Footer.js

--- src/main/frontend/src/components/Footer.js
+++ src/main/frontend/src/components/Footer.js
@@ -1,11 +1,52 @@
 import '../styles/App.css';
 import React from 'react';
 import '../styles/globalStyles.css'
+import '../styles/Footer.css'
 
 function Footer(){
     return(
-        <div>
-
+        <div className="footer">
+            <div className="footer-logo">
+                <img src="/logo2.png" alt='logoImg' width="60" height="60"></img>
+            </div>
+            <div className="call-center">
+                <p className="color-red">CALL CENTER </p>
+                <p> (본점) 010-7753-4135</p>
+                <p> (지점) 010-2344-7239</p>
+            </div>
+            <div className="bank-info">
+                <p className="color-red">BANK INFO</p>
+                <p>기업은행 01077534135</p>
+                <p>예금주 : 강태혁</p>
+            </div>
+            <div className="company-info">
+                <p className="color-red">COMPANY INFO</p>
+                <p>Address : </p>
+                <p>(본점) : 경기도 안양시 동안구 전파로 87</p>
+                <p>(지점) : 경기도 안양시 동안구 관악대로 342번길 19-8</p>
+                <p>Tel : (본점) 080-335-0020</p>
+                <p>Tel : (지점) 080-335-0021</p>
+            </div>
+            <div className="footer-category-01">
+                <p className="color-red">CATEGORY</p>
+                <a href="/"><p>카테고리 1</p></a>
+                <a href="/"><p>카테고리 2</p></a>
+                <a href="/"><p>카테고리 3</p></a>
+                <a href="/"><p>카테고리 4</p></a>
+            </div>
+            <div className="footer-category-02">
+                <a href="/"><p>카테고리 5</p></a>
+                <a href="/"><p>카테고리 6</p></a>
+                <a href="/"><p>카테고리 7</p></a>
+                <a href="/"><p>카테고리 8</p></a>
+            </div>
+            <div className="company">
+                <p className="color-red">COMPANY</p>
+                <a href="/"><p>ABOUT US</p></a>
+                <a href="/"><p>개인정보처리방침</p></a>
+                <a href="/"><p>이용안내</p></a>
+                <a href="/"><p>이용약관</p></a>
+            </div>
         </div>
     );
 }
--- src/main/frontend/src/pages/MainPage.js
+++ src/main/frontend/src/pages/MainPage.js
@@ -5,6 +5,7 @@ import React from 'react';
 import '../styles/globalStyles.css'
 import Header from "../components/Header";
 import MainCategory from "../components/MainCategory";
+import Footer from "../components/Footer";
 
 
 function MainPage() {
@@ -13,6 +14,8 @@ function MainPage() {
         <div className="main-page">
         <Header/>
     <MainCategory/>
+            <br/>
+        <Footer/>
         </div>
     );
 }
