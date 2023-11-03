0eaaa4ecd4ea90aa346795938161f069dc1f5304
함형우 사원 <101438486+HWOwoo@users.noreply.github.com>
2023-11-03 15:05:31 +0900

    fix(MyInfo.js) : Side탭 기능 수정

--- src/main/frontend/src/pages/myinfo/MyInfo.js
+++ src/main/frontend/src/pages/myinfo/MyInfo.js
@@ -1,24 +1,54 @@
 import Header from "../../components/Header";
 import MainCategory from "../../components/MainCategory";
 import Footer from "../../components/Footer";
-import MyInfoSide from "../../components/myPage/MyInfoSide";
+
 import MyInfoContent from "../../components/myPage/MyInfoContent";
-
-
+import "../../styles/myInfo/myInfoSide.css"
+import pImage from "../../img/info/info.svg"
+import React, { useState } from "react";
 
 function MyInfo() {
+
+    const [selectedTab, setSelectedTab] = useState('profile'); // 기본값 설정
+
+    const ProfileSettings = () => <MyInfoContent />;
+    const PurchaseHistory = () => <div>구매 내역입니다.</div>;
+    const Wishlist = () => <div>찜한 상품 목록입니다.</div>;
+
     return (
         <>
-        <Header/>
-        <MainCategory/>
-        <div className="side-page">
-            <div className="side-page-content">
-                <MyInfoSide/>
-                <MyInfoContent/>
+            <Header/>
+            <MainCategory/>
+            <div className="side-page">
+                    <div className="side-page-content">
+                        <div className="myInfo-side-tab">
+                            <div className="profile-left">
+                                <div className="p-l-info">
+                                    <img className="image-icon" alt="" src={pImage} />
+                                    <p className="p-l-i-email">nelap1234@gmail.com</p>  
+                                    <b className="p-l-i-nickName">닉네임</b>                      
+                                </div>
+                            <div className="p-l-info-content">
+                                <div className="p-l-i-button">
+                                    <div className="p-l-i-tab" onClick={() => setSelectedTab('profile')}>프로필 설정</div>
+                                </div>
+                                <div className="p-l-i-button">
+                                    <div className="p-l-i-tab" onClick={() => setSelectedTab('buyList')}>구매 내역</div>
+                                </div>
+                                <div className="p-l-i-button">
+                                    <div className="p-l-i-tab" onClick={() => setSelectedTab('wishList')}>찜한 상품</div>
+                                </div>
+                                <hr className="p-l-i-hr" />
+                            </div>
+                        </div>
+                    </div>
+                {selectedTab === 'profile' && <ProfileSettings />}
+                {selectedTab === 'buyList' && <PurchaseHistory />}
+                {selectedTab === 'wishList' && <Wishlist />}
+                </div>
             </div>
-        </div>
-        <Footer/>
-    </>
+            <Footer/>
+        </>
     );
 }
 
