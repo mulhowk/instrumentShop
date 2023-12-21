27041b795a62625398f6f130ad922baea8ef725d
HWOwoo <nelap1234@naver.com>
2023-12-22 00:53:03 +0900

    feat, fix : 마이페이지 접속계정 반영, 주소지 관리 기능 개발

---
+++ src/main/java/com/example/instrumentshop/Address/DTO/AddressInfoDTO.java
@@ -0,0 +1,2 @@
+package com.example.instrumentshop.Address.DTO;public class AddressInfoDTO {
+}
---
+++ src/main/java/com/example/instrumentshop/Address/DTO/AddressResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.instrumentshop.Address.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressResponeDTO {
+    private Long addressId;
+    private String addressReceiver;
+    private String addressNickname;
+    private String memberPhone;
+    private String addressValue;
+    private String addressPostnumber;
+}
